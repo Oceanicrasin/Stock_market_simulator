d6966b473a901fdcd7689f8090d3ab44aacfce7e
oceanic555rasin <georgethomasmrf@gmail.com>
2026-02-05 18:39:00 +0000

    Iteration 5.5 Manual table ordering

--- Main.py
+++ Main.py
@@ -32,10 +32,14 @@ def popup(message):
 class HomeScreen(Screen):
     def deletion(self):
         # clears database then rebuilds it
-        database.remove_table("companies")
-        database.remove_table("traders")
+        database.print_db("companies")
+        database.print_db("traders")
+        database.print_db("current_positions")
+        database.print_db("transaction_history")
+        database.clear_db()
         database.init_db()
         database.repopulate_db()
+
 
 class FinancialStatementScreen(Screen):
     def on_enter(self):
@@ -87,12 +91,90 @@ class PlaceOrderScreen(Screen):
 
         capital -= required_capital
         database.change_capital(ticker, capital)
+        database.insert_into_order_book(ticker,share_price,num_of_shares,required_capital)
+        database.print_db("buy_order_book")
         message = "Successfully placed order. Current Capital: " + str(capital)
         popup(message)
 
 
+
+    def sell(self):
+
+        # ensure all values are the correct type
+        try:
+            ticker = int(self.ids.sell_company_input.text)
+            num_of_shares = float(self.ids.sell_num_of_shares_input.text)
+            share_price = int(self.ids.sell_share_price_input.text)
+        except ValueError:
+            popup("Invalid data type entered\n All values must be numbers")
+            return
+        # ensure all values are not negative
+        if ticker < 0 or num_of_shares < 0 or share_price < 0:
+            popup("Negative value entered")
+            return
+        # ensure ticker is within range
+        if ticker < 0 or ticker > 11:
+            popup("Invalid ticker entered")
+            return
+        # check if user has a sufficient amount of shares
+        positions = database.extract_table("current_positions")
+        for position in positions:
+            if position[1] == ticker:
+                if position[3] >= num_of_shares:
+                    message = "Successfully placed order."
+                    popup(message)
+                    database.add_order("sell", ticker, share_price, num_of_shares, share_price*num_of_shares)
+                    database.print_db("sell_order_book")
+                    return
+                else:
+                    popup("You do not own enough of this stock")
+                    return
+        popup("You do not own this stock")
+        return
+
+
+class PortfolioScreen(Screen):
+    def get_Capital(self):
+        capital = database.extract_table("traders")[0][1]
+        return str(capital)
+
+    def on_enter(self):
+        # runs every time you enter this screen
+        positions = database.extract_table("current_positions")
+        self.ids.portfolio_table.clear_widgets()
+        # headings for the table
+        self.ids.portfolio_table.add_widget(Label(text="Trader_id",color=(0,0,0,1)))
+        self.ids.portfolio_table.add_widget(Label(text="Ticker",color=(0,0,0,1)))
+        self.ids.portfolio_table.add_widget(Label(text="Avg_price",color=(0,0,0,1)))
+        self.ids.portfolio_table.add_widget(Label(text="Num_of_shares",color=(0,0,0,1)))
+        self.ids.portfolio_table.add_widget(Label(text="Total Value",color=(0,0,0,1)))
+        self.ids.portfolio_table.add_widget(Label(text="Profit",color=(0,0,0,1)))
+        # inserting data into the table
+        for position in positions:
+            for data in position:
+                self.ids.portfolio_table.add_widget(Label(text=str(data),color=(0,0,0,1)))
+
+
 class WindowManager(ScreenManager):
     pass
+
+class OrderBook(Screen):
+    def on_enter(self):
+        # runs every time you enter this screen
+        buy_orders = database.extract_table("buy_order_book")
+        self.ids.buy_order_table.clear_widgets()
+        # headings for the table
+        self.ids.buy_order_table.add_widget(Label(text="Buy_order_id",color=(0,0,0,1)))
+        self.ids.buy_order_table.add_widget(Label(text="Trader_id",color=(0,0,0,1)))
+        self.ids.buy_order_table.add_widget(Label(text="Ticker",color=(0,0,0,1)))
+        self.ids.buy_order_table.add_widget(Label(text="Share_price",color=(0,0,0,1)))
+        self.ids.buy_order_table.add_widget(Label(text="Num_of_shares",color=(0,0,0,1)))
+        self.ids.buy_order_table.add_widget(Label(text="total_value",color=(0,0,0,1)))
+        # inserting data into the table
+        for order in buy_orders:
+            for data in order:
+                self.ids.buy_order_table.add_widget(Label(text=str(data),color=(0,0,0,1)))
+
 
 
 kv = Builder.load_file("kivy.kv")
--- database.py
+++ database.py
@@ -3,7 +3,9 @@ import sqlite3
 DB_NAME = "Stock_market_db"
 
 def get_connection():
-    return sqlite3.connect(DB_NAME)
+    connection = sqlite3.connect(DB_NAME)
+    connection.execute("PRAGMA foreign_keys = ON;")
+    return connection
 
 def init_db():
     # Creates all the tables required for the program
@@ -29,7 +31,98 @@ def init_db():
        capital REAL NOT NULL
        )
      """)
-    connection.commit()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS current_positions (
+     trader_id INTEGER NOT NULL,
+     ticker INTEGER NOT NULL,
+     avg_price REAL NOT NULL,
+     num_of_shares INTEGER NOT NULL,
+     total_value REAL NOT NULL,
+     profit REAL NOT NULL,
+     
+     PRIMARY KEY (trader_id,ticker),
+     
+     FOREIGN KEY (trader_id)
+     REFERENCES traders (trader_id)
+     ON DELETE CASCADE
+     ON UPDATE CASCADE,
+     
+     FOREIGN KEY (ticker)
+     REFERENCES companies (ticker)
+     ON DELETE CASCADE
+     ON UPDATE CASCADE
+    )
+    """)
+
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS transaction_history (
+     transaction_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+     trader_id INTEGER NOT NULL,
+     transaction_type VARCHAR NOT NULL,
+     ticker INTEGER NOT NULL,
+     share_price REAL NOT NULL,
+     num_of_shares INTEGER NOT NULL,
+     total_value REAL NOT NULL,
+     
+     FOREIGN KEY (trader_id) 
+     REFERENCES traders (trader_id)
+     ON DELETE CASCADE
+     ON UPDATE CASCADE,
+
+     FOREIGN KEY (ticker) 
+     REFERENCES companies (ticker)
+     ON DELETE CASCADE
+     ON UPDATE CASCADE
+    )
+    """)
+
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS buy_order_book (
+     buy_order_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+     trader_id INTEGER NOT NULL,
+     ticker INTEGER NOT NULL,
+     share_price REAL NOT NULL,
+     num_of_shares INTEGER NOT NULL,
+     total_value REAL NOT NULL,
+     
+     FOREIGN KEY (trader_id) 
+     REFERENCES traders (trader_id)
+     ON DELETE CASCADE
+     ON UPDATE CASCADE,
+
+     FOREIGN KEY (ticker) 
+     REFERENCES companies (ticker)
+     ON DELETE CASCADE
+     ON UPDATE CASCADE
+     )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS sell_order_book (
+     sell_order_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+     trader_id INTEGER NOT NULL,
+     ticker INTEGER NOT NULL,
+     share_price REAL NOT NULL,
+     num_of_shares INTEGER NOT NULL,
+     total_value REAL NOT NULL,
+
+     FOREIGN KEY (trader_id) 
+     REFERENCES traders (trader_id)
+     ON DELETE CASCADE
+     ON UPDATE CASCADE,
+
+     FOREIGN KEY (ticker) 
+     REFERENCES companies (ticker)
+     ON DELETE CASCADE
+     ON UPDATE CASCADE
+     )
+    """)
+
+
+
     cursor.close()
 
 def repopulate_db():
@@ -38,7 +131,7 @@ def repopulate_db():
     cursor = connection.cursor()
     cursor.execute("""SELECT * FROM companies """)
     companies = cursor.fetchall()
-    print(companies)
+
     if not companies:
         cursor.execute(""" 
         INSERT INTO companies
@@ -90,33 +183,69 @@ def repopulate_db():
                 """)
     cursor.execute("""SELECT * FROM traders """)
     traders = cursor.fetchall()
-    print(traders)
     if not traders:
         cursor.execute(""" 
         INSERT INTO traders
         VALUES (0,100000)
         """)
+    cursor.execute(""" SELECT * FROM current_positions """)
+    current_positions = cursor.fetchall()
+    cursor.execute(""" SELECT * FROM transaction_history """)
+    transaction_history = cursor.fetchall()
+    if not transaction_history and not current_positions:
+        cursor.execute(""" 
+        INSERT INTO current_positions VALUES (0,0,1,1000,1000,0)
+         """)
+        cursor.execute(""" 
+        INSERT INTO current_positions VALUES (0,1,1,1000,1000,0)
+         """)
+        cursor.execute(""" 
+        INSERT INTO current_positions VALUES (0,2,1,1000,1000,0)
+         """)
+        cursor.execute(""" 
+        INSERT INTO transaction_history VALUES (0,0,'BUY',0,1,1000,1000)
+         """)
+        cursor.execute(""" 
+        INSERT INTO transaction_history VALUES (1,0,'BUY',1,1,1000,1000)
+         """)
+        cursor.execute(""" 
+        INSERT INTO transaction_history VALUES (2,0,'BUY',2,1,1000,1000)
+         """)
+
     connection.commit()
     cursor.close()
 
 
-def remove_table(table):
+def clear_db():
     # drops a table
     connection = get_connection()
     cursor = connection.cursor()
-    cursor.execute("""DROP TABLE IF EXISTS {}""".format(table))
+
+    cursor.execute("""DROP TABLE IF EXISTS buy_order_book""")
+    cursor.execute("""DROP TABLE IF EXISTS sell_order_book""")
+    cursor.execute("""DROP TABLE IF EXISTS current_positions""")
+    cursor.execute("""DROP TABLE IF EXISTS transaction_history""")
+    cursor.execute("""DROP TABLE IF EXISTS companies""")
+    cursor.execute("""DROP TABLE IF EXISTS traders""")
+    connection.commit()
+    cursor.close()
+
+def clear_buy_order_book():
+    connection = get_connection()
+    cursor = connection.cursor()
+    cursor.execute("""DROP TABLE IF EXISTS buy_order_book""")
     connection.commit()
     cursor.close()
 
 
-def print_db():
+def print_db(table):
     #outputs all the contents of a table to the terminal
     connection = get_connection()
     cursor = connection.cursor()
-    cursor.execute("""SELECT * FROM companies """)
-    companies = cursor.fetchall()
-    for company in companies:
-        print(company)
+    cursor.execute("""SELECT * FROM {} """.format(table))
+    data = cursor.fetchall()
+    for record in data:
+        print(record)
 
 def extract_table(table):
     # returns a 2d list containing all the contents of a table
@@ -132,5 +261,72 @@ def change_capital(trader_id, capital):
     cursor.execute(""" UPDATE traders SET capital = {} WHERE trader_id = {} """.format(capital, trader_id))
     connection.commit()
     cursor.close()
+
+def add_order(type, ticker, share_price, num_of_shares, total_value):
+    connection = get_connection()
+    cursor = connection.cursor()
+    if type == "buy":
+        cursor.execute(""" 
+        INSERT INTO buy_order_book (trader_id, ticker, share_price, num_of_shares, total_value) 
+        VALUES (?, ?, ?, ?, ?)
+        """, (0, ticker, share_price, num_of_shares, total_value))
+        connection.commit()
+    else:
+        cursor.execute(""" 
+        INSERT INTO sell_order_book (trader_id, ticker, share_price, num_of_shares, total_value) 
+        VALUES (?, ?, ?, ?, ?)
+        """, (0, ticker, share_price, num_of_shares, total_value))
+        connection.commit()
+
+
+def insert_into_order_book(ticker, share_price, num_of_shares, total_value):
+    connection = get_connection()
+    cursor = connection.cursor()
+    cursor.execute(""" SELECT * FROM buy_order_book""")
+    item = [ticker, share_price, num_of_shares, total_value]
+    order_book = cursor.fetchall()
+    print("Order book Initial:", order_book)
+    # delete the order_ids:
+    for record in order_book:
+        record.pop(0)
+    print("Order book without id:", order_book)
+    i = 0
+    added = False
+    for record in order_book:
+        # causes order book to be ordered by ascending ticker
+        if ticker < record[1]:
+            insert(item,order_book,i)
+            added = True
+            break
+        elif ticker == record[1]:
+            if share_price <= record[2]:
+                #causes order book to be ordered by ascending share price
+                insert(item,order_book,i)
+                added = True
+                break
+    if not added:
+        order_book.append(item)
+
+    #add records into database
+    print("Order book with info: ",order_book)
+    clear_buy_order_book()
+    init_db()
+    for record in order_book:
+        add_order("buy",record[0],record[1],record[2],record[3])
+
+
+def insert(item,lst,index):
+    n_lst=[]
+    for i in range(0,len(lst)-1):
+        if i == index:
+            n_lst.append(item)
+        n_lst.append(lst[i])
+    return n_lst
+
+# a = insert(10,[1,2,3,4],1)
+# print (a)
+
+print_db("buy_order_book")
+
 
 
